79e1ff7457244a76a1335ffd9c601007fe9e858c
kushagragarwal2443 <kushagragarwal2443@gmail.com>
2021-09-23 20:49:48 +0530

    Test Case added

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,5 @@
 const Netflix = artifacts.require("Netflix");
+
     
     module.exports = function (deployer) {
       deployer.deploy(Netflix);
--- test/test1.js
+++ test/test1.js
@@ -7,40 +7,37 @@ const bpublic = buyerid.publicKey;
 const bprivate = buyerid.privateKey;
 const spublic = sellerid.publicKey;
 const sprivate = sellerid.privateKey;
-const item1 = "screen1";
-const item2 = "screen2";
+const item1 = "I_am_the_Password_for_Screen1";
+const item2 = "I_am_the_Password_for_Screen2";
 
 contract("Netflix", (accounts) => {
     let [buyer, seller] = accounts;
     it("simulates a whole transaction", async () => {
         const contractInstance = await Netflix.new();
-        console.log("Starting test");
+        console.log("\n\nStarting test");
         let avail = await contractInstance.viewAvailItems({from: buyer});
         console.log(avail);
-        console.log("Listing new item.")
-        await contractInstance.listItem("screen 1", "new Netflix Screen", 10, {from: seller});
-        await contractInstance.listItem("screen 2", "new Netflix Screen", 5, {from: seller});
+        console.log("\nListing new item.")
+        await contractInstance.listItem("Screen 1", "new Netflix Screen", 100000, {from: seller});
+        await contractInstance.listItem("Screen 2", "The Best Netflix Screen", 500000, {from: seller});
         avail = await contractInstance.viewAvailItems({from: buyer});
         console.log(avail);
-        await contractInstance.buyItem(0, bpublic, {from: buyer, value:10});
-        let pk = await contractInstance.get_public_key(0, {from: seller});
+        console.log("\nBuyer liked Screen 2, hence he is buying it")
+        await contractInstance.buyItem(1, bpublic, {from: buyer, value:500000});
+        console.log("\nScreen 2 should now be removed from the listing, lets check")
+        avail = await contractInstance.viewAvailItems({from: buyer});
+        console.log(avail);
+        let pk = await contractInstance.get_public_key(1, {from: seller});
         assert.equal(pk, bpublic);
-        const encrypted = await EthCrypto.encryptWithPublicKey(
-            pk,
-            item1
-          );
-        console.log("Encrypted string:")
+        const encrypted = await EthCrypto.encryptWithPublicKey(pk, item2);
+        console.log("\nEncrypted string:")
         console.log(encrypted)
-        await contractInstance.send_encrypted_string(EthCrypto.cipher.stringify(encrypted) ,0, {from: seller});
-        let es = await contractInstance.get_encrypted_string(0, {from: buyer});
+        await contractInstance.send_encrypted_string(EthCrypto.cipher.stringify(encrypted) ,1, {from: seller});
+        let es = await contractInstance.get_encrypted_string(1, {from: buyer});
         assert.equal(es, EthCrypto.cipher.stringify(encrypted));
-        const decrypted = await EthCrypto.decryptWithPrivateKey(
-            bprivate,
-            EthCrypto.cipher.parse(es)
-          );
-        console.log("Decrypted string:")
+        const decrypted = await EthCrypto.decryptWithPrivateKey(bprivate, EthCrypto.cipher.parse(es));
+        console.log("\nDecrypted string:")
         console.log(decrypted)
-        assert.equal(decrypted, item1);
-        
+        assert.equal(decrypted, item2);
     })
 })
